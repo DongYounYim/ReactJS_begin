ca1035e03b168a587c39d8d19e52835d3c70f89c
Im_younique <48540492+DongYounYim@users.noreply.github.com>
2022-05-16 01:24:35 +0900

    Make simple coin selecter App

--- create_app/first-reactapp/src/App.js
+++ create_app/first-reactapp/src/App.js
@@ -1,33 +1,69 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [selected, setSelect] = useState(1);
+  const initobj = {
+    name: "plz select",
+    symbol: "",
+    quotes: {
+      USD: {
+        price: 0.1
+      }
     }
-    setToDo("");
-    setToDos(currentArray => [toDo, ...currentArray])
+  }
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((reponse) => reponse.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+      setCoins(current => [initobj, ...current]);
+    });
+  }, [])
+  const onChange = (event) => {
+    setMoney(event.target.value);
+  }
+  const sChange = (event) => {
+    setSelect(event.target.value);
   }
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          onChange={onChange} 
-          value = {toDo}
-          type="text" 
-          placeholder="Write your to do.." 
-        />
-        <button>Add to do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? 
+        <strong>Loading...</strong> : 
+        (
+        <select onChange={sChange}>
+          {coins.map((coin) => (
+            <option value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <br>
+      </br>
+      {loading ?
+        "" :
+        (
+        <input
+          onChange={onChange}
+          type = "number"
+          placeholder = "your dollar($)"
+        >
+        </input>
+      )}
+      {loading ?
+        "" :
+        (
+          <input
+          value={money === 0 ? "" : money/selected}
+          >
+          </input>
+        )
+      }
     </div>
   );
 }
