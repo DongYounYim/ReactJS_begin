b75fd78290640026772a864edf821fbe9535745e
Im_younique <48540492+DongYounYim@users.noreply.github.com>
2022-05-19 00:47:18 +0900

    Done Chapetr#7.5Component

--- create_app/first-reactapp/src/App.js
+++ create_app/first-reactapp/src/App.js
@@ -1,70 +1,15 @@
-import { useState, useEffect } from "react"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  const [selected, setSelect] = useState(1);
-  const initobj = {
-    name: "plz select",
-    symbol: "",
-    quotes: {
-      USD: {
-        price: 0.1
-      }
-    }
-  }
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((reponse) => reponse.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);
-      setCoins(current => [initobj, ...current]);
-    });
-  }, [])
-  const onChange = (event) => {
-    setMoney(event.target.value);
-  }
-  const sChange = (event) => {
-    setSelect(event.target.value);
-  }
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? 
-        <strong>Loading...</strong> : 
-        (
-        <select onChange={sChange}>
-          {coins.map((coin) => (
-            <option value={coin.quotes.USD.price}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-      <br>
-      </br>
-      {loading ?
-        "" :
-        (
-        <input
-          onChange={onChange}
-          type = "number"
-          placeholder = "your dollar($)"
-        >
-        </input>
-      )}
-      {loading ?
-        "" :
-        (
-          <input
-          value={money === 0 ? "" : money/selected}
-          >
-          </input>
-        )
-      }
-    </div>
+  <Router>
+    <Routes>
+      <Route path="/hello" element={<h1>Hello</h1>} />
+      <Route path="/movie" element={<Detail />}/>
+      <Route path="/" element={<Home />}/>
+    </Routes>
+  </Router>
   );
 }
 
